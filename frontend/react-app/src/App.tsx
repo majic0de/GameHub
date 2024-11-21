1aa512377b606e0f678e7ca9962937d4325cb17c
Masoud Majidi <masoud.majidi.engineer@gmail.com>
2024-11-21 11:42:53 -0500

    Build a responsive layout

--- frontend/react-app/src/App.tsx
+++ frontend/react-app/src/App.tsx
@@ -1,9 +1,24 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Button>Hello!</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area={"nav"} bg={"coral"}>
+          Nav
+        </GridItem>
+        <GridItem area={"aside"} bg={"gold"} hideBelow="lg">
+          Aside
+        </GridItem>
+        <GridItem area={"main"} bg={"dodgerblue"}>
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 }

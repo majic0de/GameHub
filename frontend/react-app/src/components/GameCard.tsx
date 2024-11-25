a17a080b2a1151c927b093a775fc35ec5e69ac15
Masoud Majidi <masoud.majidi.engineer@gmail.com>
2024-11-25 12:19:34 -0500

    Get optimized images

--- frontend/react-app/src/components/GameCard.tsx
+++ frontend/react-app/src/components/GameCard.tsx
@@ -2,12 +2,13 @@ import { Game } from "@/hooks/useGames";
 import { Card, HStack, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "@/services/image-url";
 
 const GameCard = ({ game }: { game: Game }) => {
   return (
     <>
       <Card.Root overflow={"hidden"}>
-        <Image src={game.background_image}></Image>
+        <Image src={getCroppedImageUrl(game.background_image)}></Image>
         <Card.Body>
           <Card.Title>{game.name}</Card.Title>
           <HStack justifyContent={"space-between"}>

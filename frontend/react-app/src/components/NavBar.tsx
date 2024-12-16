c9fd5378ac0a222092145a656b41457405e434d9
Masoud Majidi <masoud.majidi.engineer@gmail.com>
2024-12-16 11:28:01 -0500

    Create the search input

--- frontend/react-app/src/components/NavBar.tsx
+++ frontend/react-app/src/components/NavBar.tsx
@@ -1,12 +1,14 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import { ColorModeButton } from "./ui/color-mode";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
     <>
       <HStack justifyContent={"space-between"} padding="10px">
-        <Image src={logo} boxSize="60px"></Image>
+        <Image src={logo} boxSize="60px" />
+        <SearchInput />
         <ColorModeButton />
       </HStack>
     </>
---
+++ frontend/react-app/src/components/SearchInput.tsx
@@ -0,0 +1,17 @@
+import { Input } from "@chakra-ui/react";
+import { InputGroup } from "./ui/input-group";
+import { BsSearch } from "react-icons/bs";
+
+const SearchInput = () => {
+  return (
+    <InputGroup flex={1} startElement={<BsSearch />}>
+      <Input
+        placeholder="Search games..."
+        borderRadius={20}
+        variant={"subtle"}
+      />
+    </InputGroup>
+  );
+};
+
+export default SearchInput;
